39d7a141d408042fe5852297239990e73497723b
yordanos wuletaw <101830198+J0RdN-w@users.noreply.github.com>
2023-01-23 19:42:17 +0300

    Update 36. Valid Sudoku.py

--- 36. Valid Sudoku.py
+++ 36. Valid Sudoku.py
@@ -1,16 +1,14 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
 
-        rowSet = {}
-        colSet = {}
+        rows = defaultdict(set)
+        cols = defaultdict(set)
 
         def validateSudoku(rowStart, rowEnd, colStart, colEnd):
-
             # hashSet to check for repetition in 3x3 matrix
             hashSet = set()
             for i in range(rowStart, rowEnd):
                 for j in range(colStart, colEnd):
-
                     if board[i][j] != '.':
                         # if digit is between 1 - 9 and unique
                         if int(board[i][j]) < 0 or int(board[i][j]) > 9 or board[i][j] in hashSet:
@@ -18,21 +16,16 @@ class Solution:
                         hashSet.add(board[i][j])
                     
                         # check if digits in row are unique
-                        if i in rowSet:  
-                            if board[i][j] in rowSet[i]:
+                        if i in rows:  
+                            if board[i][j] in rows[i]:
                                 return False
-                            rowSet[i].add(board[i][j]) 
-                        else:
-                            rowSet[i] = set(board[i][j])
+                        rows[i].add(board[i][j]) 
                         
                         # check if digits in column are unique
-                        if j in colSet:
-                            if board[i][j] in colSet[j]:
+                        if j in cols:
+                            if board[i][j] in cols[j]:
                                 return False
-                            colSet[j].add(board[i][j]) 
-                        else:
-                            colSet[j] = set(board[i][j])
-                        
+                        cols[j].add(board[i][j]) 
                         
             return True
         
