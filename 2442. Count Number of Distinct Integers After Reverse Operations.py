6cfa905825dab041f787c2151c4785c220953c94
yordanos wuletaw <101830198+J0RdN-w@users.noreply.github.com>
2023-01-25 16:16:19 +0300

    Create 2442. Count Number of Distinct Integers After Reverse Operations.py

---
+++ 2442. Count Number of Distinct Integers After Reverse Operations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countDistinctIntegers(self, nums: List[int]) -> int:
+
+        hashSet = set()
+
+        for i in range(len(nums)):
+            hashSet.add(nums[i])
+            hashSet.add(self.reverse(nums[i]))
+        
+        return len(hashSet)
+        
+    def reverse(self, num):
+        res = 0
+        while num:
+            res *= 10
+            res += (num % 10)
+            num = num // 10
+        return res
