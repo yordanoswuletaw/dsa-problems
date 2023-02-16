bf88f02ca70d8e1a93ac72e11e1a00f57473a54f
yordanos wuletaw <101830198+J0RdN-w@users.noreply.github.com>
2023-02-16 21:14:07 +0300

    Update 2410. Maximum Matching of Players With Trainers.py

--- 2410. Maximum Matching of Players With Trainers.py
+++ 2410. Maximum Matching of Players With Trainers.py
@@ -3,11 +3,10 @@ class Solution:
         players.sort(reverse=True)
         trainers.sort(reverse=True)
 
-        len1, len2 = len(players), len(trainers)
         ptr1= ptr2 = 0
         maxMachings = 0
 
-        while ptr1 < len1 and ptr2 < len2:
+        while ptr1 < len(players) and ptr2 < len(trainers):
             if players[ptr1] <= trainers[ptr2]:
                 maxMachings += 1
                 ptr1 += 1
