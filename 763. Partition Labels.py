b36182d49c73586360559653d17ba047c64237f0
yordanos wuletaw <101830198+J0RdN-w@users.noreply.github.com>
2023-02-19 12:04:58 +0300

    Update 763. Partition Labels.py

--- 763. Partition Labels.py
+++ 763. Partition Labels.py
@@ -1,23 +1,36 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
 
-        n, i = len(s), 0
+        last = {c: i for i, c in enumerate(s)}
+        end, breakPt = 0, -1
         output = []
 
-        while i < n:
-            # last index of character at index i
-            lastIndx = s.rfind(s[i])
+        for i in range(len(s)):
+            end = max(end, last[s[i]])
+            if i == end:
+                output.append(i - breakPt)
+                breakPt = i
+            i += 1
 
-            indx = i + 1
-            # updating last index 
-            while indx < lastIndx:
-                lastIndx = max(lastIndx, s.rfind(s[indx]))
-                indx += 1
-            lastIndx += 1
-            output.append(lastIndx - i)
-            i = lastIndx 
-        
         return output
+
+        # n, i = len(s), 0
+        # output = []
+
+        # while i < n:
+        #     # last index of character at index i
+        #     lastIndx = s.rfind(s[i])
+
+        #     indx = i + 1
+        #     # updating last index 
+        #     while indx < lastIndx:
+        #         lastIndx = max(lastIndx, s.rfind(s[indx]))
+        #         indx += 1
+        #     lastIndx += 1
+        #     output.append(lastIndx - i)
+        #     i = lastIndx 
+        
+        # return output
                 
                 
 
