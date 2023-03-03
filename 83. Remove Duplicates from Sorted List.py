2066411afb4267973a185029978ffa9bcbcdd42e
yordanos wuletaw <101830198+J0RdN-w@users.noreply.github.com>
2023-03-03 21:07:59 +0300

    Create 83. Remove Duplicates from Sorted List.py

---
+++ 83. Remove Duplicates from Sorted List.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        ptr = head
+        while ptr and ptr.next:
+            if ptr.val == ptr.next.val:
+                ptr.next = ptr.next.next
+            else:
+                ptr = ptr.next
+                
+        return head
