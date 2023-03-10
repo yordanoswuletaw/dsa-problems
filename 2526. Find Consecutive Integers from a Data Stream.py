6cf2d8d72caf058292128210296930b4a81bb678
yordanos wuletaw <101830198+J0RdN-w@users.noreply.github.com>
2023-03-10 11:26:27 +0300

    Update 2526. Find Consecutive Integers from a Data Stream.py

--- 2526. Find Consecutive Integers from a Data Stream.py
+++ 2526. Find Consecutive Integers from a Data Stream.py
@@ -4,11 +4,8 @@ class DataStream:
     def __init__(self, value: int, k: int):
         self.value = value
         self.k = k
-        self.queue = deque()
         # to track a number which equals to self.vaue
-        self.eqValue = 0
-        # to track a number which is not equal to value
-        # self.notValue = -1    
+        self.eqValue = 0   
 
     def consec(self, num: int) -> bool:
         if num == self.value:
@@ -17,18 +14,6 @@ class DataStream:
             self.eqValue = 0
         return self.eqValue >= self.k
 
-        # using Queue Ds
-        # deque the queue if it is full
-        # if len(self.queue) == self.k:
-        #     self.queue.popleft() 
-        #     self.notValue -= 1
-        # self.queue.append(num)
-        # # if we added not value number in the queue rare we reset self.notValue to k again
-        # if num != self.value:
-        #     self.notValue = len(self.queue) - 1
-        # if len(self.queue) < self.k or self.notValue >= 0:
-        #     return False 
-        # return True 
 
 
 # Your DataStream object will be instantiated and called as such:
