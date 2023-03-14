2e57db248536d6752977f6840d908e80d4a7917f
yordanos wuletaw <101830198+J0RdN-w@users.noreply.github.com>
2023-03-14 20:12:32 +0300

    Update 344. Reverse String.py

--- 344. Reverse String.py
+++ 344. Reverse String.py
@@ -3,9 +3,20 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        leftPtr, rightPtr = 0, len(s) - 1
-        while leftPtr < rightPtr:
-            s[leftPtr], s[rightPtr] = s[rightPtr], s[leftPtr]
-            leftPtr += 1
-            rightPtr -= 1
+        # using iteration
+        # leftPtr, rightPtr = 0, len(s) - 1
+        # while leftPtr < rightPtr:
+        #     s[leftPtr], s[rightPtr] = s[rightPtr], s[leftPtr]
+        #     leftPtr += 1
+        #     rightPtr -= 1
+
+        # using recursion
+        self.reverse(s, 0, len(s) - 1)
+
+    def reverse(self, s, left, right):
+        if left >= right:
+            return
+        s[left], s[right] = s[right], s[left]
+        self.reverse(s, left + 1, right - 1)
+    
         
