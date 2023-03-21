6d95df1c197496ade8d85520a2b871928d6ffd2f
yordanos wuletaw <101830198+J0RdN-w@users.noreply.github.com>
2023-03-21 14:57:12 +0300

    Create 1011. Capacity To Ship Packages Within D Days.py

---
+++ 1011. Capacity To Ship Packages Within D Days.py
@@ -0,0 +1,32 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+
+        low, heigh = 1, sum(weights)
+        leastWeight = heigh
+
+        if days == 1:
+            return heigh
+        elif days == len(weights):
+            return max(weights)
+
+        while low <= heigh:
+            mid = low + (heigh - low) // 2
+            maxWeight = currDay = weightSum = 0
+            for weight in weights:
+                if weightSum + weight > mid:
+                   currDay += 1
+                   maxWeight = max(maxWeight, weightSum)
+                   weightSum = 0
+                weightSum += weight
+            if weightSum :
+                currDay += 1
+            
+            print(mid, currDay, maxWeight)
+            
+            if currDay <= days:
+                leastWeight = min(maxWeight, leastWeight) 
+                heigh = mid - 1
+            else:
+                low = mid + 1
+        
+        return leastWeight
