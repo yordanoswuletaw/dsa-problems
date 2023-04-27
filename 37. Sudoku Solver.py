b2af1012ae3aae870c6f41d489d86372c08d9c67
yordanos wuletaw <101830198+J0RdN-w@users.noreply.github.com>
2023-04-27 23:07:48 +0300

    Create 37. Sudoku Solver.py

---
+++ 37. Sudoku Solver.py
@@ -0,0 +1,55 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        self.found = False
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        subBoard = defaultdict(set)
+        
+        # adding each digit in row, column and sub-boxes hashset
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+                    subBoard[(i //3, j //3)].add(board[i][j])
+        
+
+        def solve(row, col):
+            # validation or base case
+            if row > 8:
+                self.found =  True
+                return
+
+            for j in range(col, 9):
+                if board[row][j] == '.':
+                    for val in map(str, range(1, 10)):
+                        if val not in rows[row] and val not in cols[j] and val not in subBoard[(row//3, j//3)]:
+                            board[row][j] = val
+                            rows[row].add(val)
+                            cols[j].add(val)
+                            subBoard[(row//3, j//3)].add(val)
+                            if j + 1 > 8:
+                                solve(row + 1, 0)
+                            else:
+                                solve(row, j + 1)
+                            if not self.found:
+                                board[row][j] = '.'
+                                rows[row].remove(val)
+                                cols[j].remove(val)
+                                subBoard[(row//3,j//3)].remove(val)
+
+                    # if no valid digit found    
+                    if board[row][j] == '.':
+                        return
+            # if we completed the current row with valid digits
+            if not self.found:
+                solve(row + 1, 0) 
+
+        solve(0,0)
+            
+
+        
+       
