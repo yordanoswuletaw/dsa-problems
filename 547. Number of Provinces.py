e2058c822bf5a6f49b9c97a5ad7d9e21f2d8b6a9
yordanos wuletaw <101830198+J0RdN-w@users.noreply.github.com>
2023-05-19 20:27:14 +0300

    Update 547. Number of Provinces.py

--- 547. Number of Provinces.py
+++ 547. Number of Provinces.py
@@ -1,30 +1,49 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
 
-        n, m = len(isConnected), len(isConnected[0])
-        adjList = defaultdict(list)
-        for i in range(n):
-            for j in range(m):
-                if isConnected[i][j]:
-                    if i != j:
-                        adjList[i].append(j)
-                    elif i not in adjList:
-                        adjList[i] = []
-
+        # solution one
+        n = len(isConnected)
         visited = set()
         connected = 0
 
-        def dfs(vertix):
-            if vertix in visited:
-                return 
-
-            visited.add(vertix)    
-            for neigbr in adjList[vertix]:
-                dfs(neigbr)
+        def dfs(start):
+            visited.add(start)
+            for dest in range(n):
+                if dest not in visited and isConnected[start][dest]:
+                    dfs(dest)
         
-        for vertix in adjList.keys():
-            if vertix not in visited:
-                dfs(vertix)
+        for city in range(n):
+            if city not in visited and isConnected[city][city]:
+                dfs(city)
                 connected += 1
         
         return connected
+
+        # solution two
+        # n, m = len(isConnected), len(isConnected[0])
+        # adjList = defaultdict(list)
+        # for i in range(n):
+        #     for j in range(m):
+        #         if isConnected[i][j]:
+        #             if i != j:
+        #                 adjList[i].append(j)
+        #             elif i not in adjList:
+        #                 adjList[i] = []
+
+        # visited = set()
+        # connected = 0
+
+        # def dfs(vertix):
+        #     if vertix in visited:
+        #         return 
+
+        #     visited.add(vertix)    
+        #     for neigbr in adjList[vertix]:
+        #         dfs(neigbr)
+        
+        # for vertix in adjList.keys():
+        #     if vertix not in visited:
+        #         dfs(vertix)
+        #         connected += 1
+        
+        # return connected
