8148b155f0985af6e5cf917bc18874a22b51212f
J0RdN w <101830198+yordanoswuletaw@users.noreply.github.com>
2025-04-04 09:36:10 +0300

    Time: 3 ms (57.53%) | Memory: 18.3 MB (25.86%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
@@ -7,33 +7,48 @@
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
 
-        hashmap = {}
-        queue = deque([root])
-        deepest_leaf = set([root])
-
-        while queue:
-            next_deepest_leaf = set()
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                if node.left:
-                    queue.append(node.left)
-                    next_deepest_leaf.add(node.left)
-                    hashmap[node.left] = node
-                if node.right:
-                    queue.append(node.right)
-                    next_deepest_leaf.add(node.right)
-                    hashmap[node.right] = node
+        def dfs(node, depth):
+            if not node:
+                return node, depth
             
-            if next_deepest_leaf:
-                deepest_leaf = next_deepest_leaf
+            l_node, l_depth = dfs(node.left, depth + 1)
+            r_node, r_depth = dfs(node.right, depth + 1)
 
-        while len(deepest_leaf) > 1:
-            next_leaf = set()
-            for _ in range(len(deepest_leaf)):
-                node = deepest_leaf.pop()
-                next_leaf.add(hashmap[node])
-            deepest_leaf = next_leaf
+            if l_depth == r_depth:
+                return (node, r_depth)
+            
+            return (l_node, l_depth) if l_depth >= r_depth else (r_node, r_depth)
         
-        return deepest_leaf.pop()
+        node, depth = dfs(root, 1 - 1)
+        return node
+
+        # hashmap = {}
+        # queue = deque([root])
+        # deepest_leaf = set([root])
+
+        # while queue:
+        #     next_deepest_leaf = set()
+        #     for _ in range(len(queue)):
+        #         node = queue.popleft()
+        #         if node.left:
+        #             queue.append(node.left)
+        #             next_deepest_leaf.add(node.left)
+        #             hashmap[node.left] = node
+        #         if node.right:
+        #             queue.append(node.right)
+        #             next_deepest_leaf.add(node.right)
+        #             hashmap[node.right] = node
+            
+        #     if next_deepest_leaf:
+        #         deepest_leaf = next_deepest_leaf
+
+        # while len(deepest_leaf) > 1:
+        #     next_leaf = set()
+        #     for _ in range(len(deepest_leaf)):
+        #         node = deepest_leaf.pop()
+        #         next_leaf.add(hashmap[node])
+        #     deepest_leaf = next_leaf
+        
+        # return deepest_leaf.pop()
 
         
